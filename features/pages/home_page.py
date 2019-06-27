90edaa687af4ac85da1a08fd50e0106890330b63
Marcio Lemos <GERU\marcio@ger-nt-qua-133.geru.local>
2019-06-27 17:53:02 -0300

    fix import

--- features/pages/home_page.py
+++ features/pages/home_page.py
@@ -1,8 +1,5 @@
-from selenium import webdriver
-
 from selenium.webdriver.common.by import By
 from features.helpers.base_page import BasePage
-
 
 class HomePageLocator(object):
     INPUT_ITEM = (By.NAME, 'as_word')
