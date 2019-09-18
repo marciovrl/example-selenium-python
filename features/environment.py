783ed72eafe573d2598d107fb67cc0c50ad0a29b
marcio lemos <contato.marciovrl@gmail.com>
2019-09-18 13:22:58 -0300

    add report Allure

--- features/environment.py
+++ features/environment.py
@@ -1,8 +1,12 @@
+import os
+import allure
+
 from features.helpers.browser import Browser
+from allure_commons.types import AttachmentType
 
 
 def before_all(context):
-    print ("before all")
+    print ("Starting Testing")
 
 
 def before_scenario(context, scenario):
@@ -11,9 +15,16 @@ def before_scenario(context, scenario):
 
 
 def after_scenario(context, scenario):
+    if scenario.status == "failed":
+        screenshot_file = os.getcwd() + "/report-allure/" + \
+            scenario.name.replace(" ", "_").lower() + ".png"
+        context.browser.save_screenshot(screenshot_file)
+        allure.attach(context.browser.get_screenshot_as_png(),
+                      name="Screenshot", attachment_type=AttachmentType.PNG)
+
     if context.browser is not None:
         context.browser.close()
 
 
 def after_all(context):
-    print ("after all")
+    print ("Finish Testing")
