da75db3a574102a765527abc60a04ef74739ed0f
marcio lemos <contato.marciovrl@gmail.com>
2020-02-16 10:46:02 -0300

    update search_item.py

--- features/steps/search_item.py
+++ features/steps/search_item.py
@@ -1,8 +1,8 @@
-from features.pages.home_page import HomePage
-from features.pages.result_page import ResultPage
+from features.pages.home import HomePage
+from features.pages.list import ListPage
 
 
-@step(u'that it is on the homepage of Mercado Livre')
+@step('that it is on the homepage of Mercado Livre')
 def step_impl(context):
     if not hasattr(context, 'home_page'):
         context.home_page = HomePage(context.browser)
@@ -10,28 +10,27 @@ def step_impl(context):
     context.home_page.navigate_to(context.config.userdata['url'])
 
 
-@step(u'I search for "{item}"')
+@step('I search for "{item}"')
 def step_impl(context, item):
     context.home_page.search_item(item)
 
 
-@step(u'I view items according to my search')
+@step('I view items according to my search')
 def step_impl(context):
-    if not hasattr(context, 'result_page'):
-        context.result_page = ResultPage(context.browser)
+    if not hasattr(context, 'list_page'):
+        context.list_page = ListPage(context.browser)
 
-    assert context.result_page.is_item_searched_displayed('golf') is True
+    assert 'golf' in context.list_page.is_item_searched_displayed()
 
 
-@step(u'Select "{option}" option')
+@step('I select "{option}" option')
 def step_impl(context, option):
     context.home_page.go_to_menu_item(option)
 
 
-@step(u'I see Deals of the week')
+@step('I see deals')
 def step_impl(context):
-    if not hasattr(context, 'result_page'):
-        context.result_page = ResultPage(context.browser)
+    if not hasattr(context, 'list_page'):
+        context.list_page = ListPage(context.browser)
 
-    assert context.result_page.is_page_the_selected(
-        'Ofertas da semana') is True
+    assert 'Ofertas' in context.list_page.is_page_the_selected()
