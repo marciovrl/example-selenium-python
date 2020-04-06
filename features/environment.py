36d1e3247813c2e466f22746366c04093df88326
marcio lemos <contato.marciovrl@gmail.com>
2020-04-06 10:53:33 -0300

    removed allure code

--- features/environment.py
+++ features/environment.py
@@ -1,8 +1,5 @@
 import os
-import allure
-
 from features.helpers.browser import Browser
-from allure_commons.types import AttachmentType
 
 
 def before_all(context):
@@ -15,13 +12,6 @@ def before_scenario(context, scenario):
 
 
 def after_scenario(context, scenario):
-    if scenario.status == 'failed':
-        screenshot_file = os.getcwd() + '/report-allure/' + \
-            scenario.name.replace(' ', '_').lower() + '.png'
-        context.browser.save_screenshot(screenshot_file)
-        allure.attach(context.browser.get_screenshot_as_png(),
-                      name='Screenshot', attachment_type=AttachmentType.PNG)
-
     if context.browser is not None:
         context.browser.close()
 
